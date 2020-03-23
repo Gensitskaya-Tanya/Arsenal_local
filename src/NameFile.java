22d930f18963f29e549747418b0f51549d23a0b6
Gensitskaya-Tanya <gensitskaya.tanya@gmail.com>
2020-03-23 19:29:43 +0200

    new changes

--- src/NameFile.java
+++ src/NameFile.java
@@ -1,4 +1,6 @@
 public class NameFile {
-    public static final String FILE_NAME_READ = "G:\\Информация по расчету фаз\\тестирование частей кода\\639x194badSignal2.xlsx";
-    public static final String FILE_NAME_WRITE = "G:\\Информация по расчету фаз\\программа для СЫРЫХ ДАННЫХ\\step 8 averige- 10 5000\\100x470Signal.xlsx";
+    public static final String FILE_NAME_READ = "H:\\измерения март 2020\\2020_03_06 лазерное зеркало 2\\2-я матрица\\x499-y261-3,812613 3.7525.xlsx";
+    public static final String FILE_NAME_WRITE = "C:\\Users\\Tanya\\Downloads\\тестирование AppNew\\AppNew.xlsx";
+    public static final String BIN_FILE_NAME_READ = "C:\\Users\\Tanya\\Downloads\\ско по 10 точкам 3 ряда\\5000.bin";
+
 }
